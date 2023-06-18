1105a3dd14ad6d17f3400cb993f3848fe5760da7
barbarabrito <barbaramabrito@gmail.com>
2023-06-18 16:40:03 -0300

    fix: remove adblock banner from html

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import puppeteer from 'puppeteer';
 import { engine } from 'express-handlebars';
 import bodyParser from 'body-parser';
+import * as cheerio from 'cheerio';
 
 const app = express();
 
@@ -49,6 +50,9 @@ const browse = async (url: string) => {
 
   await page.goto(url);
   const html = await page.content();
+  const $ = cheerio.load(html);
+
+  $("detecta-adblock").remove();
   await browser.close();
   
   return html;
